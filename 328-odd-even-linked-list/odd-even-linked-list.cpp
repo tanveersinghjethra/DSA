a017011db94a2ac9c93b0f9a007d938e45aa8728
Tanveer Singh Jethra <tanveerjethra3@gmail.com>
2026-07-07 21:21:47 +0530

    Time: 0 ms (100.00%) | Memory: 15.5 MB (63.21%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.cpp
+++ 328-odd-even-linked-list/odd-even-linked-list.cpp
@@ -1,41 +1,20 @@
-/** * Definition for singly-linked list. * struct ListNode { * int val; * ListNode *next; * ListNode() : val(0), next(nullptr) {} * ListNode(int x) : val(x), next(nullptr) {} * ListNode(int x, ListNode *next) : val(x), next(next) {} * }; */
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
+        if (head == nullptr || head->next == nullptr) return head;
 
-        if(head==NULL || head->next==NULL)
-            return head;
+        ListNode* odd = head;
+        ListNode* even = head->next;
+        ListNode* evenHead = even;
 
-        vector<int> arr;
-
-        ListNode* temp = head;
-
-        // Odd positions
-        while(temp && temp->next){
-            arr.push_back(temp->val);
-            temp = temp->next->next;
-        }
-        if(temp)
-            arr.push_back(temp->val);
-
-        // Even positions
-        temp = head->next;
-        while(temp && temp->next){
-            arr.push_back(temp->val);
-            temp = temp->next->next;
-        }
-        if(temp)
-            arr.push_back(temp->val);
-
-        // Copy back
-        temp = head;
-        int i = 0;
-
-        while(temp){
-            temp->val = arr[i++];
-            temp = temp->next;
+        while (even != nullptr && even->next != nullptr) {
+            odd->next = even->next;
+            odd = odd->next;
+            even->next = even->next->next;
+            even = even->next;
         }
 
+        odd->next = evenHead; // Connect odd list to even list
         return head;
     }
 };
