896f2e64c479810d1cf51fb8979750e296f5fc7f
Tanveer Singh Jethra <tanveerjethra3@gmail.com>
2026-09-02 14:45:39 +0530

    Time: 0 ms (100.00%) | Memory: 30.1 MB (94.95%) - LeetSync

---
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        // int n=nums1.size();
+        // vector<int>nums2(n);
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<n;j++){
+        //         if(nums2[i]==nums1[i] && nums2[j]!=nums1[j])return true;
+        //         else if(nums2[i] == nums1[i] - nums1[j])return true;
+        //     }
+        // }
+        return true;
+    }
+};
